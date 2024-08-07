573f4580b2a3fe68e9e5608f269086ad2381f962
Luis Benavides <luis.benavides@collectivehealth.com>
2024-08-07 14:50:23 -0400

    adding some lombok annotations

--- src/main/java/com/cleveritgroup/orderdivider/entity/Address.java
+++ src/main/java/com/cleveritgroup/orderdivider/entity/Address.java
@@ -1,12 +1,15 @@
 package com.cleveritgroup.orderdivider.entity;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class Address {
 
     private String street;
     private String city;
     private String state;
     private String zoneId;
+    private String country;
 }
--- src/main/java/com/cleveritgroup/orderdivider/entity/Order.java
+++ src/main/java/com/cleveritgroup/orderdivider/entity/Order.java
@@ -1,5 +1,7 @@
 package com.cleveritgroup.orderdivider.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
@@ -11,6 +13,8 @@ import java.util.List;
 @Document
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Order {
     @Id
     private String orderId;
