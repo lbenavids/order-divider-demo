96df97a7554304ca128c553a7d685da189560dc3
Luis Benavides <luis.benavides@collectivehealth.com>
2024-08-07 15:04:03 -0400

    adding some lombok annotations part 3

--- src/main/java/com/cleveritgroup/orderdivider/entity/Person.java
+++ src/main/java/com/cleveritgroup/orderdivider/entity/Person.java
@@ -1,10 +1,16 @@
 package com.cleveritgroup.orderdivider.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
     private String firstName;
     private String lastName;
